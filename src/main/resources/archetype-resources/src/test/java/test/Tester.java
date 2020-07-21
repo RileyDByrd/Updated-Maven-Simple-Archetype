f8810f677d05b7d2515f4f229672eb8205ec0a08
WhiteHatHacker1 <eonsahead@protonmail.com>
2020-07-20 17:17:49 -0700

    Commit of first release

---
+++ src/main/resources/archetype-resources/src/test/java/test/Tester.java
@@ -0,0 +1,13 @@
+package ${package}.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Tester 
+{
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
